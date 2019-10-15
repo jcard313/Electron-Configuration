e99aaa1ae067ca2089ff38159713f16ec04ff129
jcard313 <56241132+jcard313@users.noreply.github.com>
2019-10-15 19:19:02 -0400

    Add files via upload

--- Electron Configuration.py
+++ Electron Configuration.py
@@ -39,11 +39,11 @@ electronList = ["1s^1", "1s^2", "2s^1", "2s^2", "2p^1", "2p^2", "2p^3", "2p^4", 
                 "3d^5", "3d^6", "3d^7", "3d^8", "3d^9", "3d^10", "4p^1", "4p^2", "4p^3", "4p^4", "4p^5", "4p^6",
                 "5s^1", "5s^2", "4d^1", "4d^2", "4d^3", "4d^4", "4d^5", "4d^6", "4d^7", "4d^8", "4d^9", "4d^10",
                 "5p^1", "5p^2", "5p^3", "5p^4", "5p^5", "5p^6", "6s^1", "6s^2", "4f^1", "4f^2", "4f^3", "4f^4",
-                "4f^5", "4f^6", "4f^7", "4f^8", "4f^9", "4f^10", "5d^1", "5d^2", "5d^3", "5d^4", "5d^5", "5d^6",
-                "5d^7", "5d^8", "5d^9", "5d^10", "6p^1", "6p^2", "6p^3", "6p^4", "6p^5", "6p^6", "7s^1", "7s^2",
-                "5f^1", "5f^2", "5f^3", "5f^4", "5f^5", "5f^6", "5f^7", "5f^8", "5f^9", "5f^10", "6d^1", "6d^2",
-                "6d^3", "6d^4", "6d^5", "6d^6", "6d^7", "6d^8", "6d^9", "6d^10", "7p^1", "7p^2", "7p^3", "7p^4", "7p^5",
-                "7p^6"]
+                "4f^5", "4f^6", "4f^7", "4f^8", "4f^9", "4f^10", "4f^11", "4f^12", "4f^13", "4f^14", "5d^1", "5d^2",
+                "5d^3", "5d^4", "5d^5", "5d^6","5d^7", "5d^8", "5d^9", "5d^10", "6p^1", "6p^2", "6p^3", "6p^4", "6p^5",
+                "6p^6", "7s^1", "7s^2","5f^1", "5f^2", "5f^3", "5f^4", "5f^5", "5f^6", "5f^7", "5f^8", "5f^9", "5f^10",
+                "5f^11", "5f^12", "5f^13", "5f^14", "6d^1", "6d^2", "6d^3", "6d^4", "6d^5", "6d^6", "6d^7", "6d^8",
+                "6d^9", "6d^10", "7p^1", "7p^2", "7p^3", "7p^4", "7p^5","7p^6"]
 elementsAb = ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K",
               "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb",
               "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs",
@@ -63,7 +63,7 @@ elementsFull = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon",
                 "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium",
                 "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "Mendelevium",
                 "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium"]
-# defines the atomic number before starting the main funciton. looping boolean to ensure that while loops loop
+# defines the atomic number before starting the main function. looping boolean to ensure that while loops loop
 atomicNumber = 0
 looping = True
 
@@ -80,27 +80,23 @@ while looping:
     if choice == "atomic number":
         atomicNumber = input("Please input the atomic number of your element: ")
 # validates that the input is a positive integer value
-        if not atomicNumber.isnumeric or atomicNumber == "0":
-            atomicNumber = input("Invalid input. Please enter a positive integer value.")
+        while not atomicNumber.isnumeric() or atomicNumber == "0":
+            atomicNumber = input("Invalid input. Please enter a positive integer value: ")
         atomicNumber = int(atomicNumber)
-        # print(atomicNumber)
         break
     elif choice == "element name":
         elementName = input("Please input the name of your element (abbreviated or full): ").capitalize()
 # validates the input is a valid element name
-        if elementName not in elementsAb and elementName not in elementsFull:
+        while elementName not in elementsAb and elementName not in elementsFull:
             elementName = input("Element name not recognized. Please try again.").capitalize()
 # checks that elementName is in the lists of elementsAb or elementsFull. If it is, then the index of the element in that list
 # is used as the atomicNumber
-        while True:
-            try:
-                atomicNumber = elementsAb.index(elementName)
-            except ValueError:
-                atomicNumber = elementsFull.index(elementName)
-            break
+        try:
+            atomicNumber = elementsAb.index(elementName)
+        except ValueError:
+            atomicNumber = elementsFull.index(elementName)
 # you must add 1 to the atomic number as the index starts at 0, but the atomic number starts at 1
         atomicNumber = atomicNumber + 1
-        # print(elementName)
         break
     else:
         input("Invalid input. Please type ""atomic number"" or ""element name"". Press any button to continue")
@@ -111,11 +107,19 @@ while looping:
     if not charge.lstrip("+-").isnumeric():
         charge = input("You have entered an invalid charge. Please input a valid charge: ")
     charge = int(charge)
+    #if not 20 < atomicNumber <= 30 or 38 < atomicNumber < 48 or 88 < atomicNumber < 98:
+    #if charge > 0:
+    #    if 20 < atomicNumber <= 30:
+    #        for i in range(2):
+    #            n = 20
+    #            electronList.pop(n)
+    #            n -= 1
+    #            if n == 18: break
     atomicNumber = atomicNumber - charge
     break
 
 # prints the name of the element you have calculated, its electron configuration, and the condensed electron configuration
 print("The name of your element is:", elementsFull[atomicNumber + charge - 1])
 print("Your full electron configuration is: \n" + ' '.join(map(str, electronList[0:atomicNumber])))
-print("Your condensed electron configuration is: \n" + nobleGases(len(electronList[0:atomicNumber])) + " " +
-      ' '.join(map(str, electronList[atomicRounder(atomicNumber):atomicNumber])))
+print(("Your condensed electron configuration is: \n" + nobleGases(len(electronList[0:atomicNumber])) + " " +
+      ' '.join(map(str, electronList[atomicRounder(atomicNumber):atomicNumber]))).strip())
